6d092c315102d7ed87b436f5becd0d1dc45da054
BAUR <baurdotnet@gmail.com>
2020-03-24 16:09:54 +0600

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const epxress = require('express');
 
 const app = epxress();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json);
 
 app.listen(3000, () => {
   console.log('server is running');
@@ -26,4 +29,25 @@ app.get('/users/:id', (req, res) => {
     message: 'got one user',
     user: req.params.id,
   });
+});
+
+app.post('/login', (req, res) => {
+  const { username } = req.body;
+  const { password } = req.body;
+
+  const mockUsername = 'billyTheKid';
+  const mockPassword = 'superSecret';
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: 'password and username match!',
+      token: 'encrypted token goes here',
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'password and username not match!',
+    });
+  }
 });
