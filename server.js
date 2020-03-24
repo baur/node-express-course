e89487e60a312cbde8c5923f4c6bdb9b6f6be4ad
BAUR <baurdotnet@gmail.com>
2020-03-24 15:20:48 +0600

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const epxress = require('express');
+
 const app = epxress();
-app.listen(3000, function () {
-    console.log("server is running")
-})
+
+app.listen(3000, () => {
+  console.log('server is running');
+});
+
+const mockUserData = [
+  { name: 'Mark' },
+  { name: 'Jill' },
+];
+
+app.get('/users', (req, res) => {
+  res.json({
+    success: true,
+    message: 'sucessfully got users. Nice!',
+    users: mockUserData,
+  });
+});
